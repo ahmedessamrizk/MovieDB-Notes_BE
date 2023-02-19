168871156fc591166c2eff26b1ec6401b0fd17a5
ahmedessamrizk <105478629+ahmedessamrizk@users.noreply.github.com>
2023-02-19 21:54:26 +0200

    Update user.js

--- src/modules/user/controller/user.js
+++ src/modules/user/controller/user.js
@@ -5,7 +5,7 @@ import { userModel } from './../../../../DB/models/user.model.js';
 export const getProfile = asyncHandler(
     async (req, res, next) => {
         const { _id } = req.user;
-        const user = await findById({ model: userModel, filter: { _id: _id }, select: "first_name last_name email age" });
+        const user = await findById({ model: userModel, filter: { _id: _id }, select: "first_name last_name email age movies tvShows" });
         return res.status(200).json({ message: "Done", user });
     })
 
