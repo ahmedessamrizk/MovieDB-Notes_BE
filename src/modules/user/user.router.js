a7c20c6f77e57eab736dec967a150c5c72b32697
ahmedessamrizk <105478629+ahmedessamrizk@users.noreply.github.com>
2023-02-19 19:32:37 +0200

    Update user.router.js

--- src/modules/user/user.router.js
+++ src/modules/user/user.router.js
@@ -9,5 +9,6 @@ router.post('/addMovie', auth() ,userController.addMovie);
 router.post('/removeMovie', auth() ,userController.removeMovie);
 router.post('/addTvShow', auth() ,userController.addTvShow);
 router.post('/removeTvShow', auth() ,userController.removeTvShow);
+router.get('/getWishList', auth() ,userController.getWishList);
 
 export default router
